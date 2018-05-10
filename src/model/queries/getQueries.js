02e749bf0ab27e08c8fe47951ca27d78d7cd2eef
tspeed90 <tspeed90@gmail.com>
2018-05-10 18:45:57 +0100

    adds query test and test script

--- src/model/queries/getQueries.js
+++ src/model/queries/getQueries.js
@@ -1,5 +1,12 @@
+const dbConnect = require('../database/db_connect.js');
 // get username and password from users table
-
+const getUserData = username =>
+  dbConnect.query('SELECT password FROM users WHERE username=$1', [username])
+    .then((res) => {
+      console.log('res0', res[0]);
+      return res[0];
+    });
+// .catch(err => console.log(err));
 // get a symptom from symptoms table
 
 // get scale comments for symptom scale
@@ -11,3 +18,6 @@
 // get specific symptom ratings
 
 // get specific factor ratings
+
+
+module.exports = { getUserData };
