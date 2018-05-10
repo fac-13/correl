29464bc55ad5690127c1b779db2386a568694d5d
Jennah <contactjennah@gmail.com>
2018-05-10 04:22:36 +0100

    Uncomment the commented out app.use for the public folder

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.engine(
 
 app.set('port', process.env.PORT || 3000);
 // app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')))
-// app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(controllers);
 
 module.exports = app;
