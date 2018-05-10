a675ba4f04656b4e96a1254c054001e71badd0ab
tspeed90 <tspeed90@gmail.com>
2018-05-10 20:00:41 +0100

    adds symptoms and factors tests and queries

--- src/model/database/db_build.js
+++ src/model/database/db_build.js
@@ -4,14 +4,10 @@ const dbConnect = require('./db_connect.js');
 
 const sql = file => QueryFile(path.join(__dirname, file), { minify: true });
 
-let build;
-
-build = sql('./db_build.sql');
+const build = sql('./db_build.sql');
 
 const runDbBuild = () => dbConnect
-    .query(build)
-    .then()
-    .catch(e => console.error('error', e));
+  .query(build);
 
 runDbBuild();
 
--- src/model/queries/getQueries.js
+++ src/model/queries/getQueries.js
@@ -1,18 +1,23 @@
 const dbConnect = require('../database/db_connect.js');
 // get username and password from users table
-const getUserData = username =>
-  dbConnect.query('SELECT password FROM users WHERE username=$1', [username])
-    .then((res) => {
-      console.log('res0', res[0]);
-      return res[0];
-    });
-// .catch(err => console.log(err));
+const getUserData = username => dbConnect.query('SELECT password FROM users WHERE username=$1', [username])
+  .then((res) => {
+    console.log('res0', res[0]);
+    return res[0];
+  })
+  .catch(err => console.log(err));
+
 // get a symptom from symptoms table
+const getSymptoms = user_id => dbConnect.query('SELECT * FROM symptoms WHERE user_id=$1', [user_id])
+  // can catch error when query is called (can delete)
+  .catch(err => console.log(err));
 
 // get scale comments for symptom scale
 
 // get a factor from factors table
-
+const getFactors = user_id => dbConnect.query('SELECT * FROM factors WHERE user_id=$1', [user_id])
+  // can catch error when query is called (can delete)
+  .catch(err => console.log(err));
 // get scale comment for factor scale
 
 // get specific symptom ratings
@@ -20,4 +25,4 @@ const getUserData = username =>
 // get specific factor ratings
 
 
-module.exports = { getUserData };
+module.exports = { getUserData, getSymptoms, getFactors };
