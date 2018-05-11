f27230b94f8dfff305c0537d19368014efed9c9c
eadehemingway <eade.hemingway@gmail.com>
2018-05-11 11:39:57 +0100

    make dburl connections clearer with matts advice

--- src/model/database/db_connect.js
+++ src/model/database/db_connect.js
@@ -1,38 +1,16 @@
 const pgp = require('pg-promise')();
-const url = require('url');
+
 require('env2')('./config.env');
 
-let options = {};
+const DB_URL =
+  process.env.ENV === 'test'
+    ? process.env.TEST_DB_URL
+    : process.env.DB_URL;
 
-
-if (process.env.TRAVIS === 'true') {
-  options = {
-    database: 'travis_ci_test',
-    user: 'postgres',
-  };
-} else {
-  let DB_URL = process.env.DB_URL;
-
-  if (process.env.NODE_ENV === 'test') {
-    DB_URL = process.env.TEST_DB_URL;
-  }
-
-  if (!DB_URL) {
-    throw new Error('Environment variable DB_URL must be set!');
-  }
-
-  const params = url.parse(DB_URL);
-  const [username, password] = params.auth.split(':');
-  options = {
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
-    max: process.env.DB_MAX_CONNECTIONS || 2,
-    user: username,
-    password,
-    ssl: params.hostname !== 'localhost',
-  };
+if (!DB_URL) {
+  throw new Error('Environment variable DB_URL must be set!');
 }
 
-
-module.exports = pgp(options);
+module.exports = pgp({
+  connectionString: DB_URL,
+});
