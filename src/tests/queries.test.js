f921b2803a157ef985065157829ab9561a47912f
eadehemingway <eade.hemingway@gmail.com>
2018-05-11 11:00:06 +0100

    fixes tests to make travis pass

--- src/tests/queries.test.js
+++ src/tests/queries.test.js
@@ -1,48 +1,62 @@
 const runDbBuild = require('../model/database/db_build.js');
-const { getUserData } = require('../model/queries/getQueries');
-const dbConnect = require('../model/database/db_connect');
+const { getUserData, getSymptoms, getFactors } = require('../model/queries/getQueries');
 // const { postUserData } = require('../model/queries/postQueries');
 const test = require('tape');
 
-runDbBuild();
-
-test('simple test', (t) => {
-  dbConnect.query('SELECT * FROM users')
-    .then(res => res[0]).then((queryResult) => {
-      t.deepEqual(
-        Object.keys(queryResult),
-        ['id', 'username', 'password'],
-        'three keys',
+// test query to get username and password from users table
+test('testing that getUserData returns an object with user password', (t) => {
+  runDbBuild()
+    .then((res) => {
+      t.ok(res);
+      return getUserData('eade');
+    })
+    .then((queryResult) => {
+      t.equal(
+        queryResult.password,
+        'passworddd',
+        'queryResult should contain passworddd',
       );
-      t.equal(queryResult.username, 'eade', 'returns eade');
-      t.equal(queryResult.password, 'passworddd', 'should return passworddd');
+      t.end();
+    })
+    .catch((e) => {
+      t.error(e, 'error with getting user data');
       t.end();
     });
 });
 
-// test query to get username and password from users table
-test('testing that getUserData returns an object with user password', (t) => {
-  getUserData('eade').then((queryResult) => {
-    t.equal(
-      queryResult.password,
-      'passworddd',
-      'queryResult should contain passworddd',
-    );
-    t.end();
-  })
-    .catch(console.log);
-});
-
-
 // test query to get a symptom from symptoms table
+test('testing symptoms query returns something', (t) => {
+  runDbBuild()
+    .then(() => getSymptoms(1))
+    .then((queryResult) => {
+      t.ok(queryResult);
+      t.end();
+    })
+    .catch((e) => {
+      t.error(e, 'error with symptoms query');
+      t.end();
+    });
+});
 
 // test query to get scale comments for symptom scale
 
 // test query to get a factor from factors table
+test('testing factors query returns something', (t) => {
+  runDbBuild()
+    .then(() => getFactors(1))
+    .then((queryResult) => {
+      t.ok(queryResult);
+      t.end();
+    })
+    .catch((e) => {
+      t.error(e, 'error with factors query');
+      t.end();
+    });
+});
 
 // test query to get scale comment for factor scale
 
 // test query to get specific symptom ratings
 
-// test query to get specific factor ratings
 
+// test query to get specific factor ratings
--- src/tests/routes.test.js
+++ src/tests/routes.test.js
@@ -1,127 +1,180 @@
 const request = require('supertest');
-const mocha = require('mocha');
-const chai = require('chai');
-const assert = require('chai').assert;
-const expect = require('chai').expect;
-const express = require('express');
-
+const test = require('tape');
 const app = require('./../app.js');
+
 // test home route
-it('tests home returns html file with status code of 200', (done) => {
+test('tests home returns html file with status code of 200', (t) => {
   request(app)
     .get('/')
     .expect(200)
     .expect('Content-Type', /html/)
-    .end(done);
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
 });
 
 // // test login route
-it('tests login returns html file with status code of 200', (done) => {
+test('tests login returns html file with status code of 200', (t) => {
   request(app)
     .get('/logIn')
     .expect(200)
     .expect('Content-Type', /html/)
-    .end(done);
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
 });
 // // test register route
 
-it('tests register returns html file with status code of 200', (done) => {
+test('tests register returns html file with status code of 200', (t) => {
   request(app)
     .get('/register')
     .expect(200)
     .expect('Content-Type', /html/)
-    .end(done);
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
 });
 
 // test error route
 
-it('tests errors return html file with status code of 404', (done) => {
+test('tests errors return html file with status code of 404', (t) => {
   request(app)
     .get('/asdf')
     .expect(404)
     .expect('Content-Type', /html/)
-    .end(done);
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
 });
 
 // test symptom route
-describe('testing the symptom routes', () => {
-  it('tests symptoms/home return html file with status code of 200', (done) => {
-    request(app)
-      .get('/symptoms/home')
-      .expect(200)
-      .expect('Content-Type', /html/)
-      .end(done);
-  });
+test('tests symptoms/home return html file with status code of 200', (t) => {
+  request(app)
+    .get('/symptoms/home')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
+});
 
-  it('tests symptoms/new return html file with status code of 200', (done) => {
-    request(app)
-      .get('/symptoms/add')
-      .expect(200)
-      .expect('Content-Type', /html/)
-      .end(done);
-  });
-  it('tests symptoms/scaleInfo return html file with status code of 200', (done) => {
-    request(app)
-      .get('/symptoms/scaleInfo')
-      .expect(200)
-      .expect('Content-Type', /html/)
-      .end(done);
-  });
-  it('tests symptoms/scaleSetup return html file with status code of 200', (done) => {
-    request(app)
-      .get('/symptoms/scaleSetup')
-      .expect(200)
-      .expect('Content-Type', /html/)
-      .end(done);
-  });
+test('tests symptoms/new return html file with status code of 200', (t) => {
+  request(app)
+    .get('/symptoms/add')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
+});
+
+test('tests symptoms/scaleInfo return html file with status code of 200', (t) => {
+  request(app)
+    .get('/symptoms/scaleInfo')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
+});
+
+test('tests symptoms/scaleSetup return html file with status code of 200', (t) => {
+  request(app)
+    .get('/symptoms/scaleSetup')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
 });
 
 // testing factor routes
-describe('testing the factor routes', () => {
-  it('tests factor/home return html file with status code of 200', (done) => {
-    request(app)
-      .get('/factors/home')
-      .expect(200)
-      .expect('Content-Type', /html/)
-      .end(done);
-  });
-
-  it('tests factor/new return html file with status code of 200', (done) => {
-    request(app)
-      .get('/factors/add')
-      .expect(200)
-      .expect('Content-Type', /html/)
-      .end(done);
-  });
-  it('tests factor/scaleInfo return html file with status code of 200', (done) => {
-    request(app)
-      .get('/factors/scaleInfo')
-      .expect(200)
-      .expect('Content-Type', /html/)
-      .end(done);
-  });
-  it('tests factor/scaleSetup return html file with status code of 200', (done) => {
-    request(app)
-      .get('/factors/scaleSetup')
-      .expect(200)
-      .expect('Content-Type', /html/)
-      .end(done);
-  });
+test('tests factor/home return html file with status code of 200', (t) => {
+  request(app)
+    .get('/factors/home')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
 });
+
+test('tests factor/new return html file with status code of 200', (t) => {
+  request(app)
+    .get('/factors/add')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
+});
+
+test('tests factor/scaleInfo return html file with status code of 200', (t) => {
+  request(app)
+    .get('/factors/scaleInfo')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
+});
+
+test('tests factor/scaleSetup return html file with status code of 200', (t) => {
+  request(app)
+    .get('/factors/scaleSetup')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
+});
+
 // testing add data route
-it('tests adddata return html file with status code of 200', (done) => {
+test('tests adddata return html file with status code of 200', (t) => {
   request(app)
     .get('/adddata')
     .expect(200)
     .expect('Content-Type', /html/)
-    .end(done);
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
 });
 
 // testing see data route
-it('tests seedata return html file with status code of 200', (done) => {
+test('tests seedata return html file with status code of 200', (t) => {
   request(app)
     .get('/seedata')
     .expect(200)
     .expect('Content-Type', /html/)
-    .end(done);
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res);
+      t.end();
+    });
 });
