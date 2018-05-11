af58b01ae5b9758e2bc3e150d1af13565d6e1fd6
tspeed90 <tspeed90@gmail.com>
2018-05-11 10:06:05 +0100

    adds helper to add divs to symptoms scale page and show inputs on click

--- public/dom.js
+++ public/dom.js
@@ -1,1 +1,10 @@
 /* eslint-disable */
+
+var scaleButtons = document.querySelectorAll('.scale-buttons');
+
+scaleButtons.forEach(function (button) {
+  button.addEventListener('click', function () {
+    let input = button.parentNode.querySelector('input');
+    input.classList.remove('hidden');
+  });
+});
