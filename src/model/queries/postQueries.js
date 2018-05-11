8e18d3b31d8bf569022c5ba108b199a1b0cf7fa7
eadehemingway <eade.hemingway@gmail.com>
2018-05-11 14:00:51 +0100

    add post queries

--- src/model/queries/postQueries.js
+++ src/model/queries/postQueries.js
@@ -9,11 +9,15 @@ const postSymptom = (symptom, username) => dbConnect.query('insert into symptoms
 const postFactor = (factor, username) => dbConnect.query('insert into factors (factor, user_id) values ($1, (select id from users where username=$2))', [factor, username]);
 
 // post scale comments for symptom scale
-
+const postSymptomScale = (symptom, username, comment_1, comment_2, comment_3, comment_4, comment_5, comment_6, comment_7, comment_8, comment_9, comment_10) => dbConnect.query('insert into symptom_scale (symptom_id, user_id, comment_1, comment_2, comment_3, comment_4, comment_5, comment_6, comment_7, comment_8, comment_9, comment_10) values ((select id from symptoms where symptom = $1 and user_id=(select id from users where username=$2)), (select id from users where username = $2), $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)', [symptom, username, comment_1, comment_2, comment_3, comment_4, comment_5, comment_6, comment_7, comment_8, comment_9, comment_10]);
 // post scale comment for factor scale
+const postFactorScale = (factor, username, comment_1, comment_2, comment_3, comment_4, comment_5, comment_6, comment_7, comment_8, comment_9, comment_10) => dbConnect.query('insert into factor_scale (factor_id, user_id, comment_1, comment_2, comment_3, comment_4, comment_5, comment_6, comment_7, comment_8, comment_9, comment_10) values ((select id from factors where factor = $1 and user_id=(select id from users where username=$2)), (select id from users where username = $2), $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)', [factor, username, comment_1, comment_2, comment_3, comment_4, comment_5, comment_6, comment_7, comment_8, comment_9, comment_10]);
 
 // post symptom ratings
-
+const postSymptomRating = (symptom, username, rating) => dbConnect.query('insert into symptom_data (symptom_id, user_id, rating) values((select id from symptoms where symptom = $1 and user_id=(select id from users where username=$2)), (select id from users where username=$2),$3)', [symptom, username, rating]);
 // post factor ratings
+const postFactorRating = (factor, username, rating) => dbConnect.query('insert into factor_data (factor_id, user_id, rating) values((select id from factors where factor = $1 and user_id=(select id from users where username=$2)), (select id from users where username=$2),$3)', [factor, username, rating]);
 
-module.exports = { postUserData, postSymptom };
+module.exports = {
+  postUserData, postSymptom, postFactor, postSymptomScale, postFactorScale, postSymptomRating, postFactorRating,
+};
