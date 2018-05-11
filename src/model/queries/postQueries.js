c14bf30b45cd739b296384c3357761b3d478ad55
eadehemingway <eade.hemingway@gmail.com>
2018-05-11 13:21:14 +0100

    add postuserdata, postsymptom and postFactor query

--- src/model/queries/postQueries.js
+++ src/model/queries/postQueries.js
@@ -1,13 +1,19 @@
+const dbConnect = require('../database/db_connect.js');
+
 // post username and password to users table
-
+const postUserData = (username, password) => dbConnect.query('insert into users (username, password) values ($1, $2)', [username, password]);
 // post a symptom to symptom
-
-// post scale comments for symptom scale
+const postSymptom = (symptom, username) => dbConnect.query('insert into symptoms (symptom, user_id) values ($1, (select id from users where username=$2))', [symptom, username]);
 
 // post a factor to factor table
+const postFactor = (factor, username) => dbConnect.query('insert into factors (factor, user_id) values ($1, (select id from users where username=$2))', [factor, username]);
+
+// post scale comments for symptom scale
 
 // post scale comment for factor scale
 
 // post symptom ratings
 
 // post factor ratings
+
+module.exports = { postUserData, postSymptom };
