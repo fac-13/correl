b1879458bf22d45119d4752d5e5207f047b1a0e5
Jennah <contactjennah@gmail.com>
2018-05-13 14:43:03 +0100

    Add function for login post

--- src/controllers/logIn.js
+++ src/controllers/logIn.js
@@ -1,3 +1,29 @@
+const bcrypt = require('bcrypt');
+const getQueries = require('./../model/queries/getQueries');
+
+
 exports.get = (req, res) => {
   res.render('logIn', { loggedOut: true });
+};
+
+exports.post = (req, res) => {
+  console.log('gets to login post');
+  const { username } = req.body;
+  const { password } = req.body;
+  getQueries.getUserData(username)
+    .then((result) => {
+      if (result.length > 0) {
+        return bcrypt.compare(password, result[0].password);
+      }
+      return false;
+    }).then((result) => {
+      if (result) {
+        console.log('login.js');
+        req.cook.loggedIn = true;
+        req.cook.username = username;
+        res.writeHead(302, { Location: '/profile' });
+      } else {
+        res.render('login', { notmatch: true });
+      }
+    });
 };
