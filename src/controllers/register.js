1cb4aacb27122bea6ac69370591be90c5d52e52b
Jennah <contactjennah@gmail.com>
2018-05-13 14:43:53 +0100

    Add function for register post

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -1,3 +1,28 @@
+const queries = require('./../model/queries/postQueries');
+const bcrypt = require('bcrypt');
+
+
 exports.get = (req, res) => {
   res.render('register', { loggedOut: true });
+};
+
+exports.post = (req, res, next) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      console.log(`Bcrypt error is: ${err.message}`);
+      next(err);
+    } else {
+      queries
+        .addUser(username, hash)
+        .then(() => {
+          console.log('final then');
+          req.session.loggedIn = true;
+          req.session.username = req.body.username;
+          res.redirect('/profile');
+        }).catch((err) => {
+          console.log(err);
+        });
+    }
+  });
 };
