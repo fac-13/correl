9629785bbf684b78bceedf5de371a21a4d31068e
tspeed90 <tspeed90@gmail.com>
2018-05-14 15:51:38 +0100

    add scale comments to factor scales on addData page

--- public/dom.js
+++ public/dom.js
@@ -1,26 +1,36 @@
 /* eslint-disable */
 
 
-
-
-
-
 d3.select('#graph').attr('class', 'test')
 var scales = document.querySelectorAll('input[type=range]')
 // we need to use an array of comments from the database - this one is for testing only
 const test = ['No pain', '', '', '', '4 means I take some pain medication', '', '', '', 'Can\'t focus on anything but my pain', '', ''];
+const factorsTest = ['', '', 'I drank very little water - less that 500ml', '', '', '', '', '', 'Drank lots of water - Nearly 2L', ''];
 
 scales.forEach((scale) => {
     scale.addEventListener('change', function () {
-        var currentScaleId = `${this.id}`; //e.g. gives scale-fatigue
-        var commentBoxId = `comment${currentScaleId.slice(currentScaleId.indexOf('-'))}`;
-        var scaleNumberId = `${this.id}-number`;
-        var scaleNumber = document.querySelector(`#${scaleNumberId}`);
-        var commentBox = document.querySelector(`#${commentBoxId}`);
+        if (scale.parentNode.id.includes('symptom')) {
+            //Symptoms
+            var currentScaleId = `${this.id}`; //e.g. gives scale-fatigue
+            var commentBoxId = `comment${currentScaleId.slice(currentScaleId.indexOf('-'))}`;
+            var scaleNumberId = `${this.id}-number`;
+            var scaleNumber = document.querySelector(`#${scaleNumberId}`);
+            var commentBox = document.querySelector(`#${commentBoxId}`);
+
+            scaleNumber.textContent = `${this.value}`;
+            commentBox.textContent = test[this.value] === '' ? "Any comments you've saved will appear here as you move the scale" : `Hint: ${test[this.value]}`
+        } else {
+            currentScaleId = `${this.id}`; //e.g. gives scale-fatigue
+            commentBoxId = `comment${currentScaleId.slice(currentScaleId.indexOf('-'))}`;
+            scaleNumberId = `${this.id}-number`;
+            scaleNumber = document.querySelector(`#${scaleNumberId}`);
+            commentBox = document.querySelector(`#${commentBoxId}`);
+
+            scaleNumber.textContent = `${this.value}`;
+            commentBox.textContent = factorsTest[this.value] === '' ? "Any comments you've saved will appear here as you move the scale" : `Hint: ${factorsTest[this.value]}`
+        }
 
 
-        scaleNumber.textContent = `${this.value}`;
-        commentBox.textContent = test[this.value] === '' ? "Any comments you've saved will appear here as you move the scale" : `Hint: ${test[this.value]}`
     })
 })
 
