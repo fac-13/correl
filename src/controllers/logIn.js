0fe20271879c00fbadbb7ab33135d936c2efb198
eadehemingway <eade.hemingway@gmail.com>
2018-05-14 13:56:34 +0100

    test

--- src/controllers/logIn.js
+++ src/controllers/logIn.js
@@ -8,24 +8,25 @@ exports.get = (req, res) => {
 
 
 exports.post = (req, res, next) => {
-  const { username } = req.body;
-  const { password } = req.body;
-  getQueries.getUserData(username)
-    .then((result) => {
-      if (result.length > 0) {
-        return bcrypt.compare(password, result[0].password);
-      }
-      return false;
-    }).then((result) => {
-      if (result) {
-        req.session.loggedIn = true;
-        req.session.username = username;
-        res.redirect('/profile');
-      } else {
-        res.render('login', { notmatch: true });
-      }
-    }).catch((err) => {
-      console.log(err);
-      next(err);
-    });
+  console.log('sasdf');
+  // const { username } = req.body;
+  // const { password } = req.body;
+  // getQueries.getUserData(username)
+  //   .then((result) => {
+  //     if (result.length > 0) {
+  //       return bcrypt.compare(password, result[0].password);
+  //     }
+  //     return false;
+  //   }).then((result) => {
+  //     if (result) {
+  //       req.session.loggedIn = true;
+  //       req.session.username = username;
+  //       res.redirect('/profile');
+  //     } else {
+  //       res.render('login', { notmatch: true });
+  //     }
+  //   }).catch((err) => {
+  //     console.log(err);
+  //     next(err);
+  //   });
 };
