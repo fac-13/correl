98c2f5e8a8b4e1e4f65a01eafc46238dbde92a4a
Jennah <contactjennah@gmail.com>
2018-05-16 07:50:50 +0100

    Tidy up code: semi-colons, indentation

--- public/dom.js
+++ public/dom.js
@@ -51,7 +51,7 @@ var chart_width = 700;
 var chart_height =500;
 var padding = 100;
 
-var formatTime = d3.timeParse('%Y-%m-%dT%H:%M:%S.%L%Z')
+var formatTime = d3.timeParse('%Y-%m-%dT%H:%M:%S.%L%Z');
 
 
 var testData = {
@@ -121,8 +121,8 @@ var groupData = function(data, container, typePlural, type) {
 
 }
 
-var sympContainer = []
-var factContainer = []
+var sympContainer = [];
+var factContainer = [];
 
 groupData(testData, sympContainer, 'symptoms', 'symptom');
 groupData(testData, factContainer, 'factors', 'factor');
@@ -146,48 +146,50 @@ const frontRender = function (err, response) {
     // Scales
     // find minimum and maximum values
     var minSympDate =
-      d3.min(testData.symptoms, function(obj){
-      return obj.date_entered
-    })
-    var minFactDate =  d3.min(testData.factors, function(obj){
-      return obj.date_entered
-    })
-    var dates = [minSympDate, minFactDate]
-    var minDate = d3.min(dates)
-    console.log(minDate)
+      d3.min(testData.symptoms, function(obj) {
+      return obj.date_entered;
+    });
 
-    var maxSympDate =
-      d3.max(testData.symptoms, function(obj){
-      return obj.date_entered
-    })
-    var maxFactDate =  d3.max(testData.factors, function(obj){
-      return obj.date_entered
-    })
-    var dates = [maxSympDate, maxFactDate]
-    var maxDate = d3.max(dates)
+    var minFactDate =  d3.min(testData.factors, function(obj) {
+      return obj.date_entered;
+    });
+
+    var dates = [minSympDate, minFactDate];
+    var minDate = d3.min(dates);
+
+    var maxSympDate = d3.max(testData.symptoms, function(obj) {
+      return obj.date_entered;
+    });
+
+    var maxFactDate =  d3.max(testData.factors, function(obj) {
+      return obj.date_entered;
+    });
+
+    var dates = [maxSympDate, maxFactDate];
+    var maxDate = d3.max(dates);
 
     // use these min/max values to set up scales
     var x_scale = d3
       .scaleTime()
       .domain([minDate, maxDate])
-      .range([padding, chart_width - padding])
+      .range([padding, chart_width - padding]);
 
     var y_scale = d3
       .scaleLinear()
       .domain([0,10])
-      .range([chart_height - padding, padding])
+      .range([chart_height - padding, padding]);
 
     // create svg
     var svg = d3
       .select('#graph')
       .append('svg')
-      .attr('width', chart_width)
-      .attr('height', chart_height);
+        .attr('width', chart_width)
+        .attr('height', chart_height);
 
     // create axes
     var x_axis = d3
       .axisBottom(x_scale)
-      .tickFormat(d3.timeFormat('%d-%m-%Y')).ticks(d3.timeDay)
+      .tickFormat(d3.timeFormat('%d-%m-%Y')).ticks(d3.timeDay);
 
     var y_axis = d3
       .axisLeft(y_scale)
@@ -195,57 +197,57 @@ const frontRender = function (err, response) {
 
     svg
       .append('g')
-      .attr('transform', 'translate(0,' + (chart_height - padding) + ')')
+        .attr('transform', 'translate(0,' + (chart_height - padding) + ')')
       .call(x_axis);
 
     svg
       .append('g')
-      .attr('transform', 'translate(' + padding + ', 0)')
+        .attr('transform', 'translate(' + padding + ', 0)')
       .call(y_axis)
 
 
 
-    // create line
+    // Create line - used to create all other lines
     var line = d3
       .line() 
-      .x(function(d){
-        return x_scale(d.date_entered)
+      .x(function(d) {
+        return x_scale(d.date_entered);
       })
-      .y(function(d){
+      .y(function(d) {
         return y_scale(d.rating)
-      })
-
-      var colorsSymPalette = d3.scaleOrdinal(d3.schemeCategory10); 
-      var colorsFactPalette = d3.scaleOrdinal(d3.schemeSet3);
+      });
+    
+    // Create color palettes - used for line colours and legend  
+    var colorsSymPalette = d3.scaleOrdinal(d3.schemeCategory10); 
+    var colorsFactPalette = d3.scaleOrdinal(d3.schemeSet3);
       
-      
+    // Add lines for each of the symptoms to the graph  
     var symptoms = svg
       .selectAll('.symptoms')
       .data(sympContainer)
       .enter()
       .append('g')
-      .attr('class', 'symptoms')
-      .attr('class', function(d){
-        console.log('d', d)
-        return d[0].symptom
-      })
+        .attr('class', 'symptoms')
+        .attr('class', function(d) {
+          return d[0].symptom;
+        });
 
     symptoms
       .append('path')
-      .attr('fill', 'none')
-      .attr('data-legend', function(d){
-        return d.symptom
-      })
-      .style('stroke', function(d, i){
-        return colorsSymPalette(i) })
-      .attr('stroke-width', 3)
-      .attr('d', function(d){
-        return line(d)
-      })
-      .attr('class', function(d){
-        console.log('d', d)
-        return d[0].symptom
-      })
+        .attr('fill', 'none')
+        .attr('data-legend', function(d) {
+          return d.symptom;
+        })
+        .style('stroke', function(d, i) {
+          return colorsSymPalette(i); 
+        })
+        .attr('stroke-width', 3)
+        .attr('d', function(d) {
+          return line(d);
+        })
+        .attr('class', function(d) {
+          return d[0].symptom;
+        });
 
 
       // CODE TO ADD LABELS TO THE END OF THE LINES
@@ -264,113 +266,107 @@ const frontRender = function (err, response) {
     //     return d.symptom;
     //   })
 
-
+    // Add lines for each of the factors to the graph
     var factors = svg
       .selectAll('.factors')
       .data(factContainer)
       .enter()
       .append('g')
-      .attr('class', 'factors')
-      .attr('class', function(d){
-        console.log('d', d)
-        return d[0].factor
-      })
+        .attr('class', 'factors')
+        .attr('class', function(d) {
+          return d[0].factor;
+        });
 
     factors
       .append('path')
-      .attr('fill', 'none')
-      .attr('data-legend', function(d){
-        return d.factor
-      })
-      .style('stroke', function(d, i) {
-        return colorsFactPalette(i);
-      })
-      .attr('stroke-width', 3)
-      .attr('d', function(d){
-        console.log(d)
-        return line(d)
-      })
-      .attr('class', function(d){
-        console.log('d', d)
-        return d[0].factor
-      })
-
-      var sympLegend = svg
-        .selectAll('.legend')
-        .data(sympContainer)
-        .enter()
-        .append('g')
-        .attr('transform', function(d,i){
+        .attr('fill', 'none')
+        .attr('data-legend', function(d) {
+          return d.factor;
+        })
+        .style('stroke', function(d, i) {
+          return colorsFactPalette(i);
+        })
+        .attr('stroke-width', 3)
+        .attr('d', function(d) {
+          return line(d);
+        })
+        .attr('class', function(d) {
+          return d[0].factor;
+        });
+      
+    // Create legend for symptoms 
+    var sympLegend = svg
+      .selectAll('.legend')
+      .data(sympContainer)
+      .enter()
+      .append('g')
+        .attr('transform', function(d, i) {
           return "translate(" + (chart_width - 200) + "," + (i * 15 + 20) + ")";
         })
         .attr('class', '.legend')
-        .attr('class', function(d){
-          console.log('d', d)
-          return d[0].symptom
+        .attr('class', function(d) {
+          return d[0].symptom;
+        });
+
+    sympLegend
+      .append('rect')
+        .attr('width', 10)
+        .attr('height', 10)
+        .attr('fill', function(d, i) {
+          return colorsSymPalette(i); 
         })
+        .attr('class', function(d) {
+          return d[0].symptom;
+        });
 
-        sympLegend
-          .append('rect')
-          .attr('width', 10)
-          .attr('height', 10)
-          .attr('fill', function(d, i){
-          return colorsSymPalette(i) })
-          .attr('class', function(d){
-            console.log('d', d)
-            return d[0].symptom
-          })
+    sympLegend
+      .append('text')
+        .text(function(d) {
+          return d[0].symptom;
+        })
+        .attr('transform', function(d, i) {
+          return "translate(20,10)";
+        })
+        .attr('class', function(d) {
+          return d[0].symptom;
+        });
+        
+    // create legend for factors
+    var factLegend = svg
+      .selectAll('.legend')
+      .data(factContainer)
+      .enter()
+      .append('g')
+        .attr('transform', function(d, i) {
+          return "translate(" + (chart_width - 200) + "," + (i * 15 + 60) + ")";
+        })
+        .attr('class', '.legend')
+        .attr('class', function(d) {
+          return d[0].factor;
+        });
 
-        sympLegend
-            .append('text')
-            .text(function(d){
-            return d[0].symptom
-            })
-            .attr('transform', function(d,i){
-              return "translate(20,10)";
-            })
-            .attr('class', function(d){
-              console.log('d', d)
-              return d[0].symptom
-            })
-            // create legend for factors
+    factLegend
+      .append('rect')
+        .attr('width', 10)
+        .attr('height', 10)
+        .attr('fill', function(d, i) {
+          return colorsFactPalette(i); 
+        })
+        .attr('class', function(d) {
+          return d[0].factor;
+        });
 
-        var factLegend = svg
-            .selectAll('.legend')
-            .data(factContainer)
-            .enter()
-            .append('g')
-            .attr('transform', function(d,i){
-              return "translate(" + (chart_width - 200) + "," + (i * 15 + 60) + ")";
-            })
-            .attr('class', '.legend')
-            .attr('class', function(d){
-              console.log('d', d)
-              return d[0].factor
-            })
-
-            factLegend
-              .append('rect')
-              .attr('width', 10)
-              .attr('height', 10)
-              .attr('fill', function(d, i){
-              return colorsFactPalette(i) })
-              .attr('class', function(d){
-                console.log('d', d)
-                return d[0].factor
-              })
-
-            factLegend
-                .append('text')
-                .text(function(d){
-                return d[0].factor
-                })
-                .attr('transform', function(d,i){
-                  return "translate(20,10)";
-                })
-                .attr('class', function(d){
-                  console.log('d', d)
-                  return d[0].factor
-                })
+    factLegend
+      .append('text')
+        .text(function(d) {
+          return d[0].factor;
+        })
+        .attr('transform', function(d, i) {
+          return "translate(20,10)";
+        })
+        .attr('class', function(d) {
+          return d[0].factor;
+        });
 
 
       // svg
