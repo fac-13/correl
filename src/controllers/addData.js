8daf9ab2273cd60cc29fde6f1f79386c8fc07c08
tspeed90 <tspeed90@gmail.com>
2018-05-17 19:57:11 +0100

    update post queries to post rating data

--- src/controllers/addData.js
+++ src/controllers/addData.js
@@ -17,6 +17,24 @@ exports.get = (req, res) => {
 
 exports.post = (req, res) => {
   console.log('body', req.body);
-  // const promiseArray = [postSymptomRating(req.body.symptom, req.session.username), postFactorRating];
+  const keys = Object.keys(req.body);
+  const symptoms = keys.filter(key => key.startsWith('s'));
+  console.log(symptoms);
+  const symptomsRatings = Object.values(req.body);
+  const factors = keys.filter(key => key.startsWith('f'));
+  console.log(factors);
+  const factorsRating = Object.values(req.body);
+  for (let i = 0; i < keys.length; i++) {
+    const symptomsRes = getSymptoms(req.session.username)
+      .then(syms => console.log(syms));
+
+    const promiseArray = [postQueries.postSymptomRating(symptomsRes[0].symptom, req.session.username, symptomsRatings[i]), postQueries.postFactorRating(factors[i], req.session.username, factorsRating[i])];
+    Promise.all(promiseArray)
+      .then(() => {
+        // res.render('profile');
+        console.log('sent!');
+      })
+      .catch(err => console.log(err.message));
+  }
 };
 
