09d2fc7ef2195609644ae64fd79b2c2fa284947d
eadehemingway <eade.hemingway@gmail.com>
2018-05-17 09:29:49 +0100

    fix broken symptoms home page

--- src/controllers/symptoms.js
+++ src/controllers/symptoms.js
@@ -3,12 +3,14 @@ const getQueries = require('./../model/queries/getQueries');
 
 exports.getHome = (req, res) => {
   if (req.session.loggedIn) {
-    const symptomsList = {};
+    const symptomsList = [];
     return getQueries
       .getSymptoms(req.session.username)
       .then((symptoms) => {
         symptoms.forEach((symptom) => {
-          symptomsList[symptom.symptom] = symptom.symptom;
+          const obj = {};
+          obj.symptom = symptom.symptom;
+          symptomsList.push(obj);
         });
         return symptomsList;
       })
@@ -27,12 +29,12 @@ exports.getAdd = (req, res) => {
 
 exports.postAdd = (req, res) => postQueries
   .postSymptom(req.body.symptom, req.session.username)
-  .then(() => res.render('symptomsScaleSetup'))
+  .then(() => res.render('symptomScaleInfo'))
   .catch(err => console.log(err.message));
 
 exports.getScaleInfo = (req, res) => {
   if (req.session.loggedIn) {
-    res.render('scaleInfo');
+    res.render('symptomScaleInfo');
   } else {
     res.render('logIn');
   }
@@ -46,17 +48,10 @@ exports.getScaleSetup = (req, res) => {
   }
 };
 
-exports.postScaleSetup = (req, res) => {
-  console.log(req.session.username);
-  console.log(req.body);
-  return getQueries
-    .getSymptoms(req.session.username)
-    .then((symptom) => {
-      console.log(symptom);
-      return postQueries
-        .postSymptomScale(symptom[symptom.length - 1].symptom, req.session.username, req.body['1'], req.body['2'], req.body['3'], req.body['4'], req.body['5'], req.body['6'], req.body['7'], req.body['8'], req.body['9'], req.body['10']);
-    })
-    .then(() => res.render('profile'))
-    .catch(err => console.log(err.message));
-};
+exports.postScaleSetup = (req, res) => getQueries
+  .getSymptoms(req.session.username)
+  .then(symptom => postQueries
+    .postSymptomScale(symptom[symptom.length - 1].symptom, req.session.username, req.body['1'], req.body['2'], req.body['3'], req.body['4'], req.body['5'], req.body['6'], req.body['7'], req.body['8'], req.body['9'], req.body['10']))
+  .then(() => res.render('profile'))
+  .catch(err => console.log(err.message));
 
